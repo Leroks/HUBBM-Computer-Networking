acf9812ceef39864ac93189b0686bb7a45a46e8a
Taylan Özdoğan <ozdogantaylan@gmail.com>
2023-12-09 06:24:09 +0300

    Network.cpp first draft.

--- Network.cpp
+++ Network.cpp
@@ -1,34 +1,487 @@
 #include "Network.h"
+#include <sstream>
+#include <string>
+#include <ctime>
 
 Network::Network() {
 
 }
 
+std::string Network::deleteSubstring(std::string str) {
+    //delete the last # and the last character
+    while (!str.empty()) {
+        if (str.back() == '#') break;
+        str.pop_back();
+    }
+    str.pop_back();
+    //delete the first #
+    auto it = str.begin();
+    while (it != str.end()) {
+        if (*it == '#') break;
+        ++it;
+    }
+    str.erase(str.begin(), it + 1);
+    return str;
+}
+
+int Network::find_frame_size(string message, int message_limit) {
+    int size;
+    size = message.size() / message_limit;
+    if (message.size() % message_limit != 0) {
+        size++;
+    }
+    return size;
+}
+
+Client *Network::find_client(string id, vector<Client> &clients) {
+    for (auto &client: clients) {
+        if (client.client_id == id) {
+            return const_cast<Client *>(&client);
+        }
+    }
+    return nullptr;
+}
+
+Client *Network::find_client_MAC(string mac, vector<Client> &clients) {
+    for (auto &client: clients) {
+        if (client.client_mac == mac) {
+            return const_cast<Client *>(&client);
+        }
+    }
+    return nullptr;
+}
+
+string Network::find_MAC(string id, vector<Client> &clients) {
+    const Client *client = find_client(id, clients);
+    if (client) {
+        return client->client_mac;
+    } else {
+        return "";
+    }
+}
+
+void Network::print_frame(stack<Packet *> frame) {
+    PhysicalLayerPacket *pLayerPacket = dynamic_cast<PhysicalLayerPacket *>(frame.top());
+    pLayerPacket->print();
+    frame.pop();
+
+    NetworkLayerPacket *pNetworkLayerPacket = dynamic_cast<NetworkLayerPacket *>(frame.top());
+    pNetworkLayerPacket->print();
+    frame.pop();
+
+    TransportLayerPacket *pTransportLayerPacket = dynamic_cast<TransportLayerPacket *>(frame.top());
+    pTransportLayerPacket->print();
+    frame.pop();
+
+    ApplicationLayerPacket *pApplicationLayerPacket = dynamic_cast<ApplicationLayerPacket *>(frame.top());
+    pApplicationLayerPacket->print();
+
+    if (!pApplicationLayerPacket->message_data.empty()) {
+        cout << "Message chunk carried: \"" << pApplicationLayerPacket->message_data << "\"" << endl;
+    }
+
+    cout << "Number of hops so far: " << pLayerPacket->hopNumbers << endl;
+    cout << "--------" << endl;
+}
+
+void Network::putToQueue(string sender_id, string receiver_id,
+                         string message, int message_limit, const string &sender_port, const string &receiver_port,
+                         vector<Client> &clients) {
+// Find size and initialize the number
+    int frame_size = find_frame_size(message, message_limit);
+    int no = 0;
+
+    // Find the client sender and receiver
+    Client *client = find_client(sender_id, clients);
+    Client *receiver = find_client(receiver_id, clients);
+
+    // Print the title of the message
+    // Message to be sent: "A few small hops for frames, but a giant leap for this message."
+    cout << "Message to be sent: \"" << message << "\"" << endl << endl;
+
+    for (int i = 0; i < frame_size; ++i) {
+        no++;
+
+        // Message to be added
+        string data = message.substr(i * message_limit, message_limit);
+
+        // Add it to the queue
+        stack<Packet *> for_out_queue;
+        for_out_queue.push(new ApplicationLayerPacket(0, sender_id, receiver_id, data));
+        for_out_queue.push(new TransportLayerPacket(1, sender_port, receiver_port));
+        for_out_queue.push(new NetworkLayerPacket(2, client->client_ip, receiver->client_ip));
+        string findMac = find_MAC(client->routing_table[receiver_id], clients);
+        for_out_queue.push(new PhysicalLayerPacket(3, client->client_mac, findMac));
+        client->outgoing_queue.push(for_out_queue);
+
+        // Should print
+        cout << "Frame #" << no << endl;
+        print_frame(for_out_queue);
+    }
+
+    time_t currentTime = time(nullptr);
+    tm *timestamp = localtime(&currentTime);
+    ActivityType type = ActivityType::MESSAGE_SENT;
+    ostringstream oss;
+
+    oss << put_time(timestamp, "%Y-%m-%d %H:%M:%S");
+    string timestampString = oss.str();
+    Log log(timestampString, message, no, 0, sender_id, receiver_id, true, type);
+    client->log_entries.push_back(log);
+}
+
+void Network::showFrameInfo(string infoId, string outIn, int frame_no, vector<Client> &clients) {
+
+    Client *client = find_client(infoId, clients);
+    std::queue<std::stack<Packet *>> temp;
+
+    if (outIn == "out") {
+        temp = client->outgoing_queue;
+    } else {
+        temp = client->incoming_queue;
+    }
+
+    if (frame_no > temp.size()) {
+        cout << "No such frame." << endl;
+        return;
+    }
+
+    for (int i = 0; i + 1 < frame_no; i++) {
+        i++;
+        temp.pop();
+        i--;
+    }
+
+    std::stack<Packet *> frame = temp.front();
+
+    if (outIn == "out") {
+        cout << "Current Frame #" << frame_no << " on the outgoing queue of client " << infoId << endl;
+    } else {
+        cout << "Current Frame #" << frame_no << " on the incoming queue of client " << infoId << endl;
+    }
+
+    auto *pPhysicalLayerPacket = dynamic_cast<PhysicalLayerPacket *>(frame.top());
+    frame.pop();
+    auto *pLayerPacket = dynamic_cast<NetworkLayerPacket *>(frame.top());
+    frame.pop();
+    auto *pTransportLayerPacket = dynamic_cast<TransportLayerPacket *>(frame.top());
+    frame.pop();
+    auto *pApplicationLayerPacket = dynamic_cast<ApplicationLayerPacket *>(frame.top());
+
+    cout << "Carried Message: \"" << pApplicationLayerPacket->message_data << "\"" << endl;
+    cout << "Layer 0 info: Sender ID: " << pApplicationLayerPacket->sender_ID << ", Receiver ID: "
+         << pApplicationLayerPacket->receiver_ID << endl;
+    cout << "Layer 1 info: Sender port number: " << pTransportLayerPacket->sender_port_number
+         << ", Receiver port number: "
+         << pTransportLayerPacket->receiver_port_number << endl;
+    cout << "Layer 2 info: Sender IP address: " << pLayerPacket->sender_IP_address << ", Receiver IP address: "
+         << pLayerPacket->receiver_IP_address << endl;
+    cout << "Layer 3 info: Sender MAC address: " << pPhysicalLayerPacket->sender_MAC_address
+         << ", Receiver MAC address: "
+         << pPhysicalLayerPacket->receiver_MAC_address << endl;
+    cout << "Number of hops so far: " << pPhysicalLayerPacket->hopNumbers << endl;
+}
+
+void Network::showQInfo(string infoId, string outIn, vector<Client> &clients) {
+    Client *client = find_client(infoId, clients);
+    std::queue<std::stack<Packet *>> temp;
+
+    if (outIn == "out") {
+        temp = client->outgoing_queue;
+        cout << "Client " << infoId << " Outgoing Queue Status" << endl;
+    } else {
+        temp = client->incoming_queue;
+        cout << "Client " << infoId << " Incoming Queue Status" << endl;
+    }
+
+    cout << "Current total number of frames: " << temp.size() << endl;
+}
+
+bool Network::containsPunctuation(const std::string &str) {
+    const string punctuation = ".?!";
+    return str.find_first_of(punctuation) != std::string::npos;
+}
+
+bool Network::checkEnd(stack<Packet *> frame) {
+    if (frame.size() >= 3) {
+        frame.pop(); // Skip PhysicalLayerPacket
+        frame.pop(); // Skip NetworkLayerPacket
+        frame.pop(); // Skip TransportLayerPacket
+
+        ApplicationLayerPacket *pApplicationLayerPacket = dynamic_cast<ApplicationLayerPacket *>(frame.top());
+
+        if (pApplicationLayerPacket && containsPunctuation(pApplicationLayerPacket->message_data)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void Network::send(vector<Client> &clients) {
+    for (int i = 0; i < clients.size(); i++) {
+        Client *client = &clients[i];
+        queue<stack<Packet *>> queue;
+
+        if (client->outgoing_queue.empty()) {
+            continue;
+        }
+        queue = client->outgoing_queue;
+        int number;
+        number = 0;
+        while (!queue.empty()) {
+            number++;
+            stack<Packet *> frame = queue.front();
+            auto *pPhysicalLayerPacket = dynamic_cast<PhysicalLayerPacket *>(frame.top());
+            pPhysicalLayerPacket->hopNumbers++;
+            frame.pop();
+            frame.pop();
+            frame.pop();
+            auto *pApplicationLayerPacket = dynamic_cast<ApplicationLayerPacket *>(frame.top());
+            cout << "Client " << find_client_MAC(pPhysicalLayerPacket->sender_MAC_address, clients)->client_id
+                 << " sending frame #"
+                 << number << " to client "
+                 << find_client_MAC(pPhysicalLayerPacket->receiver_MAC_address, clients)->client_id << endl;
+            print_frame(queue.front());
+            char tmpChar = pApplicationLayerPacket->message_data[pApplicationLayerPacket->message_data.size() - 1];
+            if (tmpChar == '?' || tmpChar == '!' || tmpChar == '.') {
+                number = 0;
+            }
+            string to_go = client->routing_table[pApplicationLayerPacket->receiver_ID];
+            find_client(to_go, clients)->incoming_queue.push(queue.front());
+            client->outgoing_queue.pop();
+            queue = client->outgoing_queue;
+        }
+    }
+}
+
+void Network::receive(std::vector<Client> &clients) {
+    for (int i = 0; i < clients.size(); i++) {
+        Client *client = &clients[i];
+
+        std::queue<std::stack<Packet *>> temp;
+
+        if (client->incoming_queue.empty()) {
+            continue;
+        }
+
+        temp = client->incoming_queue;
+        int number;
+        number = 0;
+        std::string message;
+
+        while (!temp.empty()) {
+            number++;
+
+            std::stack<Packet *> frame = temp.front();
+
+            auto *pPhysicalLayerPacket = dynamic_cast<PhysicalLayerPacket *>(frame.top());
+            frame.pop();
+            auto *pLayerPacket = dynamic_cast<NetworkLayerPacket *>(frame.top());
+            frame.pop();
+            auto *pTransportLayerPacket = dynamic_cast<TransportLayerPacket *>(frame.top());
+            frame.pop();
+            auto *pApplicationLayerPacket = dynamic_cast<ApplicationLayerPacket *>(frame.top());
+
+            bool success = true;
+            ActivityType type = ActivityType::MESSAGE_RECEIVED;
+            bool needDel = false;
+
+            if (pApplicationLayerPacket->receiver_ID == client->client_id) {
+                message += pApplicationLayerPacket->message_data;
+                std::cout << "Client " << client->client_id << " receiving frame #" << number << " from client "
+                          << find_client_MAC(pPhysicalLayerPacket->sender_MAC_address, clients)->client_id
+                          << ", originating from client " << pApplicationLayerPacket->sender_ID << std::endl;
+                print_frame(temp.front());
+                needDel = true;
+            } else if (client->routing_table.count(client->routing_table[pApplicationLayerPacket->receiver_ID]) == 0) {
+                std::cout << "Client " << client->client_id << " receiving frame #" << number << " from client "
+                          << find_client_MAC(pPhysicalLayerPacket->sender_MAC_address, clients)->client_id
+                          << ", but intended for client " << pApplicationLayerPacket->receiver_ID << ". Forwarding... "
+                          << std::endl;
+                std::cout << "Error: Unreachable destination. Packets are dropped after "
+                          << pPhysicalLayerPacket->hopNumbers
+                          << " hops!" << std::endl;
+                type = ActivityType::MESSAGE_DROPPED;
+                success = false;
+                needDel = true;
+            } else {
+                if (number == 1) {
+                    std::cout << "Client " << client->client_id << " receiving a message from client "
+                              << find_client_MAC(pPhysicalLayerPacket->sender_MAC_address, clients)->client_id
+                              << ", but intended for client " << pApplicationLayerPacket->receiver_ID
+                              << ". Forwarding... "
+                              << std::endl;
+                }
+
+                pPhysicalLayerPacket->sender_MAC_address = client->client_mac;
+                pPhysicalLayerPacket->receiver_MAC_address =
+                        find_client(client->routing_table[pApplicationLayerPacket->receiver_ID], clients)->client_mac;
+
+                std::cout << "Frame #" << number << " MAC address change: New sender MAC "
+                          << pPhysicalLayerPacket->sender_MAC_address
+                          << ", new receiver MAC " << pPhysicalLayerPacket->receiver_MAC_address << std::endl;
+
+                std::string to_go = client->routing_table[pApplicationLayerPacket->receiver_ID];
+                client->outgoing_queue.push(temp.front());
+                type = ActivityType::MESSAGE_FORWARDED;
+            }
+
+            if (checkEnd(temp.front())) {
+                std::time_t currentTime = std::time(nullptr);
+                std::tm *timestamp = std::localtime(&currentTime);
+                std::ostringstream oss;
+
+                oss << std::put_time(timestamp, "%Y-%m-%d %H:%M:%S");
+                std::string timestampString = oss.str();
+
+                Log log(timestampString, message, number, pPhysicalLayerPacket->hopNumbers,
+                        pApplicationLayerPacket->sender_ID, pApplicationLayerPacket->receiver_ID,
+                        success, type);
+                client->log_entries.push_back(log);
+                number = 0;
+
+                if (type == ActivityType::MESSAGE_RECEIVED) {
+                    std::cout << "Client " << client->client_id << " received the message \"" << message << "\""
+                              << " from client " << pApplicationLayerPacket->sender_ID << "." << std::endl;
+                }
+
+                std::cout << "--------" << std::endl;
+                message = "";
+            }
+
+            std::stack<Packet *> tmpPack = client->incoming_queue.front();
+            if (needDel) {
+                delete tmpPack.top();
+                tmpPack.pop();
+                delete tmpPack.top();
+                tmpPack.pop();
+                delete tmpPack.top();
+                tmpPack.pop();
+                delete tmpPack.top();
+            }
+
+            client->incoming_queue.pop();
+            temp = client->incoming_queue;
+        }
+    }
+}
+
+
+void Network::print_log(string log_id, vector<Client> &clients) {
+    Client *client = find_client(log_id, clients);
+
+    std::vector<Log> &logs = client->log_entries;
+
+    if (logs.empty()) {
+        return;
+    }
+
+    std::cout << "Client " << log_id << " Logs:" << std::endl;
+
+    for (size_t i = 0; i < logs.size(); ++i) {
+        std::cout << "--------------" << std::endl;
+        std::cout << "Log Entry #" << i + 1 << ":" << std::endl;
+        logs[i].print();
+    }
+}
+
 void Network::process_commands(vector<Client> &clients, vector<string> &commands, int message_limit,
-                      const string &sender_port, const string &receiver_port) {
-    // TODO: Execute the commands given as a vector of strings while utilizing the remaining arguments.
-    /* Don't use any static variables, assume this method will be called over and over during testing.
-     Don't forget to update the necessary member variables after processing each command. For example,
-     after the MESSAGE command, the outgoing queue of the sender must have the expected frames ready to send. */
+                               const string &sender_port, const string &receiver_port) {
+    for (const auto &command: commands) {
+        int len = command.size() + 9;
+        std::stringstream ss(command);
+        std::string cmd;
+        ss >> cmd;
+
+        for (int i = 0; i < len; i++) {
+            std::cout << "-";
+        }
+        std::cout << "\nCommand: " << command << std::endl;
+        for (int i = 0; i < len; i++) {
+            std::cout << "-";
+        }
+        std::cout << std::endl;
+
+        if (cmd == "MESSAGE") {
+            std::string sender_id, receiver_id, message;
+            ss >> sender_id >> receiver_id;
+            std::getline(ss, message);
+            message = deleteSubstring(message);
+            putToQueue(sender_id, receiver_id, message, message_limit, sender_port, receiver_port, clients);
+
+        } else if (cmd == "SHOW_FRAME_INFO") {
+            std::string info_id, out_in;
+            int frame_no;
+            ss >> info_id >> out_in >> frame_no;
+            showFrameInfo(info_id, out_in, frame_no, clients);
+        } else if (cmd == "SHOW_Q_INFO") {
+            std::string info_id, out_in;
+            ss >> info_id >> out_in;
+            showQInfo(info_id, out_in, clients);
+        } else if (cmd == "SEND") {
+            send(clients);
+        } else if (cmd == "RECEIVE") {
+            receive(clients);
+        } else if (cmd == "PRINT_LOG") {
+            std::string log_id;
+            ss >> log_id;
+            print_log(log_id, clients);
+        } else {
+            std::cout << "Invalid command." << std::endl;
+        }
+    }
 }
 
 vector<Client> Network::read_clients(const string &filename) {
     vector<Client> clients;
-    // TODO: Read clients from the given input file and return a vector of Client instances.
+    string line;
+    fstream file(filename, ios::in);
+    int counter;
+    counter = 0;
+    file >> counter;
+    getline(file, line);
+    for (int i = 0; i < counter; ++i) {
+        getline(file, line);
+        stringstream ss(line);
+        string id, ip, mac;
+        ss >> id >> ip >> mac;
+        Client client(id, ip, mac);
+        clients.push_back(client);
+    }
     return clients;
 }
 
 void Network::read_routing_tables(vector<Client> &clients, const string &filename) {
-    // TODO: Read the routing tables from the given input file and populate the clients' routing_table member variable.
+    std::string line;
+    fstream file(filename, ios::in);
+    int count;
+    count = clients.size();
+    for (int i = 0; i < count; i++) {
+        for (int j = 0; j + 1 < count; j++) {
+            getline(file, line);
+            stringstream ss(line);
+            string id, id2;
+            ss >> id >> id2;
+            clients[i].routing_table[id] = id2;
+        }
+        std::getline(file, line);
+    }
 }
 
-// Returns a list of token lists for each command
 vector<string> Network::read_commands(const string &filename) {
     vector<string> commands;
-    // TODO: Read commands from the given input file and return them as a vector of strings.
+    fstream file(filename, ios::in);
+    string line;
+    getline(file, line);
+    int count = 0;
+    count = stoi(line);
+
+    for (int i = 0; i < count; ++i) {
+        getline(file, line);
+        commands.push_back(line);
+    }
     return commands;
 }
 
 Network::~Network() {
-    // TODO: Free any dynamically allocated memory if necessary.
 }
